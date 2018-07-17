ab1f6c980d46b93962001743887d1e12c3696fa6
Federico Marcos <federico.marcos@celerative.com>
2018-07-16 23:38:16 -0300

    working on db access

--- poller.go
+++ poller.go
@@ -1,7 +1,8 @@
 package main
 
-import "github.com/paulmach/go.geo"
+// import "github.com/paulmach/go.geo"
 import (
+	"github.com/lib/pq"
 	"fmt"
 	"time"
 	"sync"
@@ -10,31 +11,114 @@ import (
 	"regexp"
 	"io/ioutil"
 	"encoding/json"
+	"database/sql"
+	_ "github.com/lib/pq"
+	"os"
+	"log"
+	// "strings"
 )
 
-func testProject(){
-	p1 := geo.NewPoint(0, 0)
-	p2 := geo.NewPoint(1, 0)
-	p3 := geo.NewPoint(0.5, 1)
-	l := geo.NewLine(p1, p2)
-	proj := l.Project(p3)
-	fmt.Println(proj)
-}
+// func testProject(){
+// 	p1 := geo.NewPoint(0, 0)
+// 	p2 := geo.NewPoint(1, 0)
+// 	p3 := geo.NewPoint(0.5, 1)
+// 	l := geo.NewLine(p1, p2)
+// 	proj := l.Project(p3)
+// 	//fmt.Println(proj)
+// }
 
 var hash = ""
 var recorridoIDs []int
+var db *sql.DB
 
+// get ids from db and save into an array
 func getRecorridoIDs() {
+	type Mapping struct {
+		providerLineaID int // esto esel id de bahia? si, provider=gpsbahia
+		cualbondiLineaSlug string
+		cualbondiRecorridoIDs []int
+	}
+	// que pasa si te queres saltear un indice, o si el id del provider no viene consecutivo?
+	idMapping := []Mapping{
+		{1, "509", []int{}},
+		{4, "500", []int{}},
+		{5,  "502", []int{}},
+		{6,  "503", []int{}},
+		{7,  "504", []int{}},
+		{8,  "505", []int{}},
+		{9,  "506", []int{}},
+		{10, "507", []int{}},
+		{11, "512", []int{}},
+		{12, "513", []int{}},
+		{14, "514", []int{}},
+		{16, "517", []int{}},
+		{17, "518", []int{}},
+		{18, "519", []int{}},
+		{19, "519A", []int{}},
+		//13, 513 EX     no
+		//15, 516        no
+		//30: 520        no
+		//31: 504 EX     no
+	}
+
+	// hay dos cosas separadas, me di cuenta
+	// providerlineaid -- cualbondilineaid -- cualbondirecorridoids (multiple/2)   <-- este es el mapping
+	// recorridoid(unique) -- gpsposprev
+	var slugs = []string{}
+	for _, item := range idMapping {
+		slugs = append(slugs, item.cualbondiLineaSlug)
+	}
+	// stmt, err := db.Prepare("SELECT set_config('log_statement', 'all', true);")
+	// rows, err := stmt.Query()
+	// defer rows.Close()
+	query := `
+		SELECT
+			re.id as id
+		FROM core_recorrido re
+			JOIN core_linea li on (re.linea_id = li.id)
+			JOIN catastro_ciudad_lineas ccl on (ccl.linea_id = li.id)
+			JOIN catastro_ciudad ci on (ccl.ciudad_id = ci.id)
+		WHERE
+			ci.slug = $1
+			AND li.slug in ($2)
+	`
+	stmt, err := db.Prepare(query)
+	rows, err := stmt.Query("bahia-blanca", pq.Array(slugs))
+
+	if err != nil {
+		log.Println(query)
+		log.Fatal(err)
+	}
+	defer db.Close()
 	
+	// no entra nunca a este while
+	for rows.Next() {
+		fmt.Println(123)
+		var (
+			id int
+        )
+        if err := rows.Scan(&id); err != nil {
+			panic(err)
+        }
+        fmt.Printf("got: %d\n", id)
+	}
 }
 
 func main(){
 	var wg sync.WaitGroup
 
+	connStr := fmt.Sprintf("postgres://%s:%s@%s/%s?sslmode=disable", os.Getenv("POSTGRES_USER"), os.Getenv("POSTGRES_PASSWORD"), os.Getenv("DB_HOST"), os.Getenv("POSTGRES_DB")) // puede ser que este mal el orden en esto?
+	var err error
+	db, err = sql.Open("postgres", connStr)
+	if err != nil {
+		log.Println("ERROR1!")
+		log.Fatal(err)
+	}
+
 	// can spawn any number of goroutines in parallel, main program will never end
 	getRecorridoIDs()
-	go crawl()
-	go getHash()
+	// go crawl()
+	// go getHash()
 	// sleep forever
 	wg.Add(1)
 	wg.Wait()
@@ -89,7 +173,7 @@ type Response struct {
 
 // rid, gpsant
 // rid, gpsant
-
+// get id from a gpsping, use previous gps value
 func getRecorridoID(gps GpsPing) string {
 	return "1"
 }
@@ -111,7 +195,7 @@ func crawlOne(url string){
 	json.Unmarshal(body, &response)
 	for _, gps := range response.Data {
 		recorridoID := getRecorridoID(gps)
-		fmt.Println(recorridoID)
+		fmt.Println("recorridoID", recorridoID)
 	}
 }
 
@@ -121,28 +205,6 @@ func crawl(){
 
 	baseURL := "https://www.gpsbahia.com.ar/web/get_track_data"
 	lineas := []int{1, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 30, 31}
-
-	// 1  509
-	// 2
-	// 3  319
-	// 4  500
-	// 5  502
-	// 6  503
-	// 7  504
-	// 8  505
-	// 9  506
-	// 10 507
-	// 11 512
-	// 12 513
-	// 13 513 EX     no
-	// 14 514
-	// 15 516        no
-	// 16 517
-	// 17 518
-	// 18 519
-	// 19 519 A
-	// 30 520        no
-	// 31 504 EX     no
 
 	if hash != "" {
 		for _, lineaID := range lineas {
